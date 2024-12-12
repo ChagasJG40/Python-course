4d2e106c8e3e0b3942704e7f049c373239dc6b06
George Harisson B. Chagas <99503411+ChagasJG40@users.noreply.github.com>
2024-12-11 22:37:17 -0400

    Add files via upload

---
+++ atvoa009.py
@@ -0,0 +1,3 @@
+salario = float(input(' Qual o salário atual? R$ '))
+salárioatual = salario + (salario * 8 / 100)
+print('O salário atualizado com 8% de acréscimo, fica no valor de R$ {:.2f}'.format(salárioatual))
