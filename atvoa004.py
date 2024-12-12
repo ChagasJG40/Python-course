803f6a718b89904a9d1a1046532ef0877646b913
George Harisson B. Chagas <99503411+ChagasJG40@users.noreply.github.com>
2024-12-11 21:33:29 -0400

    Add files via upload

--- atvoa004.py
+++ atvoa004.py
@@ -1,4 +1,4 @@
-tam = int(input('Digite em metros:'))
+tam = float(input('Digite em metros:'))
 tam1 = tam * 100
 tam2 = tam * 1000
 print('O tamanho digitado em metros foi: {}, o tamanho em centimetros é {} e o tamanho em milimetros é {}'.format(tam, tam1, tam2))
