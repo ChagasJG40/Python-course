68874647407a5a9ed0ec4ed5b67a34aa327bf5c1
George Harisson B. Chagas <99503411+ChagasJG40@users.noreply.github.com>
2024-12-11 21:42:41 -0400

    Add files via upload

--- atvoa006.py
+++ atvoa006.py
@@ -1,3 +1,5 @@
 real = float(input('digite quanto você tem na carteira:'))
 dolar1 = real / 6.04
-print('O valor convertido em dolar que você tem é de {:.2f} dólares'.format(dolar1))
+Euro = real / 6.26
+Libra = real / 7.60
+print('O valor convertido em dolar na sua carteira é de {:.2f} dólares \nde Euros é de {:.2f}\ne de Libras é de {:.2f}'.format(dolar1, Euro, Libra))
